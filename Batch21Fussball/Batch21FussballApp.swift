1bfd10e2be7310a152c8384a2f95be872ceb1325
Till-Hemmerich-Syntax <till.hemmerich@syntax-institut.de>
2024-11-08 10:06:27 +0100

    Initial Commit

---
+++ Batch21Fussball/Batch21FussballApp.swift
@@ -0,0 +1,17 @@
+//
+//  Batch21FussballApp.swift
+//  Batch21Fussball
+//
+//  Created by Till Hemmerich on 08.11.24.
+//
+
+import SwiftUI
+
+@main
+struct Batch21FussballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Batch21Fussball/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Batch21Fussball
+//
+//  Created by Till Hemmerich on 08.11.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
